32218cfde469dae26ec643d0db45ec67660f4244
Narokwe <narokweaugustine@gmail.com>
2025-03-07 23:04:51 +0300

    Added deployment files

---
+++ Afyaai.py
@@ -0,0 +1,80 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+import jwt
+import datetime
+from functools import wraps
+import os
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'your_secret_key')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///afyaai.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+# User Model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    afya_tokens = db.Column(db.Float, default=0.0)
+
+# Token Authentication
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = request.headers.get('x-access-token')
+        if not token:
+            return jsonify({'message': 'Token is missing!'}), 403
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
+            current_user = User.query.filter_by(id=data['user_id']).first()
+        except:
+            return jsonify({'message': 'Token is invalid!'}), 403
+        return f(current_user, *args, **kwargs)
+    return decorated
+
+# User Registration
+@app.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+    hashed_password = generate_password_hash(data['password'], method='sha256')
+    new_user = User(username=data['username'], password=hashed_password)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({'message': 'User registered successfully'})
+
+# User Login
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    user = User.query.filter_by(username=data['username']).first()
+    if not user or not check_password_hash(user.password, data['password']):
+        return jsonify({'message': 'Invalid credentials'}), 401
+    token = jwt.encode({'user_id': user.id, 'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=1)}, app.config['SECRET_KEY'], algorithm='HS256')
+    return jsonify({'token': token})
+
+# Submit Health Data
+@app.route('/submit_data', methods=['POST'])
+@token_required
+def submit_data(current_user):
+    data = request.get_json()
+    return jsonify({'message': 'Health data submitted successfully'})
+
+# Stake Afya Tokens
+@app.route('/stake_tokens', methods=['POST'])
+@token_required
+def stake_tokens(current_user):
+    data = request.get_json()
+    stake_amount = data.get('amount', 0)
+    if stake_amount > current_user.afya_tokens:
+        return jsonify({'message': 'Insufficient Afya tokens'}), 400
+    current_user.afya_tokens -= stake_amount
+    db.session.commit()
+    return jsonify({'message': f'Successfully staked {stake_amount} Afya tokens'})
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
